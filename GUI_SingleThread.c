7b144dda8f89af7675d1ee2c593894130fcc5266
CarlosBazaldua <125518918+CarlosBazaldua@users.noreply.github.com>
2023-04-07 12:31:27 +0200

    Add files via upload

---
+++ GUI_SingleThread.c
@@ -0,0 +1,230 @@
+
+#ifdef _RTE_
+#include "RTE_Components.h"             // Component selection
+#endif
+
+#include "stm32f7xx_hal.h"
+#include "stm32746g_discovery_sdram.h"
+#include "RTE_Components.h"
+#include "cmsis_os.h"                   // ARM::CMSIS:RTOS:Keil RTX
+#include "Board_Touch.h"                // ::Board Support:Touchscreen
+#include "GUI.h"
+#include "stdio.h"
+#include "DIALOG.h"
+#include "Board_LED.h"                  // ::Board Support:LED
+#include "Driver_USART.h"               // ::CMSIS Driver:USART
+
+
+#ifdef RTE_CMSIS_RTOS_RTX
+extern uint32_t os_time;
+
+uint32_t HAL_GetTick(void) { 
+  return os_time; 
+}
+#endif
+
+
+/*********************************************************************
+* *
+Externals
+* **********************************************************************
+*/
+WM_HWIN CreateWindow(void);
+
+
+/*----------------------------------------------------------------------------
+ *      fonctions pour UART
+ *---------------------------------------------------------------------------*/
+extern ARM_DRIVER_USART Driver_USART6;
+/*----------------------------------------------------------------------------
+ *      GUIThread: GUI Thread for Single-Task Execution Model
+ *---------------------------------------------------------------------------*/
+ 
+void GUIThread (void const *argument);              // thread function
+osThreadId tid_GUIThread;                           // thread id
+osThreadDef (GUIThread, osPriorityIdle, 1, 2048);   // thread object
+
+int Init_GUIThread (void) {
+
+  tid_GUIThread = osThreadCreate (osThread(GUIThread), NULL);
+  if (!tid_GUIThread) return(-1);
+  
+  return(0);
+}
+
+
+/**
+  * System Clock Configuration
+  *   System Clock source            = PLL (HSE)
+  *   SYSCLK(Hz)                     = 200000000
+  *   HCLK(Hz)                       = 200000000
+  *   AHB Prescaler                  = 1
+  *   APB1 Prescaler                 = 4
+  *   APB2 Prescaler                 = 2
+  *   HSE Frequency(Hz)              = 25000000
+  *   PLL_M                          = 25
+  *   PLL_N                          = 400
+  *   PLL_P                          = 2
+  *   PLL_Q                          = 8
+  *   VDD(V)                         = 3.3
+  *   Main regulator output voltage  = Scale1 mode
+  *   Flash Latency(WS)              = 6
+  */
+static void SystemClock_Config (void) {
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+
+  /* Enable HSE Oscillator and activate PLL with HSE as source */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.HSIState = RCC_HSI_OFF;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 25;
+  RCC_OscInitStruct.PLL.PLLN = 400;  
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 8;
+  HAL_RCC_OscConfig(&RCC_OscInitStruct);
+
+  /* Activate the OverDrive to reach the 200 MHz Frequency */
+  HAL_PWREx_EnableOverDrive();
+  
+  /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2 clocks dividers */
+  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;  
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;  
+  HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_6);
+}
+
+
+
+/**
+  * Configure the MPU attributes
+  */
+static void MPU_Config (void) {
+  MPU_Region_InitTypeDef MPU_InitStruct;
+  
+  /* Disable the MPU */
+  HAL_MPU_Disable();
+
+  /* Configure the MPU attributes for SDRAM */
+  MPU_InitStruct.Enable = MPU_REGION_ENABLE;
+  MPU_InitStruct.BaseAddress = 0xC0200000;
+  MPU_InitStruct.Size = MPU_REGION_SIZE_2MB;
+  MPU_InitStruct.AccessPermission = MPU_REGION_FULL_ACCESS;
+  MPU_InitStruct.IsBufferable = MPU_ACCESS_NOT_BUFFERABLE;
+  MPU_InitStruct.IsCacheable = MPU_ACCESS_NOT_CACHEABLE;
+  MPU_InitStruct.IsShareable = MPU_ACCESS_NOT_SHAREABLE;
+  MPU_InitStruct.Number = MPU_REGION_NUMBER0;
+  MPU_InitStruct.TypeExtField = MPU_TEX_LEVEL1;
+  MPU_InitStruct.SubRegionDisable = 0x00;
+  MPU_InitStruct.DisableExec = MPU_INSTRUCTION_ACCESS_DISABLE;
+
+  HAL_MPU_ConfigRegion(&MPU_InitStruct);
+
+  /* Enable the MPU */
+  HAL_MPU_Enable(MPU_PRIVILEGED_DEFAULT);
+}
+
+
+/**
+  * CPU L1-Cache enable
+  */
+static void CPU_CACHE_Enable (void) {
+
+  /* Enable I-Cache */
+  SCB_EnableICache();
+
+  /* Enable D-Cache */
+  SCB_EnableDCache();
+}
+ 
+void GUIThread (void const *argument) {
+
+	WM_HWIN hDlg;
+	
+	MPU_Config ();
+	CPU_CACHE_Enable();                       /* Enable the CPU Cache           */
+  HAL_Init();                               /* Initialize the HAL Library     */
+  BSP_SDRAM_Init();                         /* Initialize BSP SDRAM           */
+  SystemClock_Config();                     /* Configure the System Clock     */
+
+  GUI_Init();
+	Touch_Initialize();
+	
+	// Call creation function for the dialog
+	hDlg = CreateWindow();
+	
+	/* Add GUI setup code here */
+
+  while (1) {
+    
+    /* All GUI related activities might only be called from here */
+		
+		// mises à jour affichage
+		GUI_Exec();
+		GUI_Delay(10);
+		GUI_X_ExecIdle();             /* Nothing left to do for the moment ... Idle processing */
+  }
+}
+
+/*********************************************************************
+*
+*       Main
+*/
+void Init_UART(void); 
+void sendCommand(char cmd, char P1,char P2);
+
+
+
+int main (void) {
+	osKernelInitialize ();                    // initialize CMSIS-RTOS
+	LED_Initialize(); 
+
+  // initialize peripherals here
+	Init_UART();
+  // create 'thread' functions that start executing,
+  Init_GUIThread();
+
+  osKernelStart ();                         // start thread execution 
+	//sendCommand(0x03, 0,3);
+
+  osDelay(osWaitForever);
+}
+
+
+void Init_UART(void){
+	Driver_USART6.Initialize(NULL);
+	Driver_USART6.PowerControl(ARM_POWER_FULL);
+	Driver_USART6.Control(	ARM_USART_MODE_ASYNCHRONOUS |
+							ARM_USART_DATA_BITS_8		|
+							ARM_USART_STOP_BITS_1		|
+							ARM_USART_PARITY_NONE		|
+							ARM_USART_FLOW_CONTROL_NONE,
+							1500); //valeur qui corresponnd pour adapter le debit a la fréquence de la carte 
+	Driver_USART6.Control(ARM_USART_CONTROL_TX,1);
+	Driver_USART6.Control(ARM_USART_CONTROL_RX,1);
+}
+
+void sendCommand(char cmd, char P1,char P2)
+{
+	int i=0;
+	unsigned char tab[12] ={0x7E ,0xFF, 0x06, 0x00,0x00,0x00,0x00,0x00,0x00,0xEF};/*"salut ca va?";*/// 
+	short checksum=0;
+
+	tab[3] = cmd;// cmd= 0x03
+	tab[5] = P1; // =0
+	tab[6]= P2; // nb son
+	checksum = 0-(tab[1]+tab[2]+tab[3]+tab[4]+tab[5]+tab[6]);
+	tab[7] = ((checksum & 0xFF00) >>8 );
+	tab[8] = checksum & 0x00FF;
+	while(Driver_USART6.GetStatus().tx_busy == 1); // attente buffer TX vide
+	Driver_USART6.Send(tab,12);
+	
+	osDelay(1000);
+	
+}
+/*************************** End of file ****************************/
+
